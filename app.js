02c367a7bf1ca5c23f09a186ec0f747d630cca6d
frank <frankcool321@gmail.com>
2022-12-22 11:57:17 +0800

    feat: init project

---
+++ app.js
@@ -0,0 +1,24 @@
+//- require related module
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+
+//- parameter setting
+const app = express();
+const port = 3000;
+
+//- set template engine
+app.engine("hbs", exphbs.engine({ defaultLayout: "main", extname: "hbs" }));
+app.set("view engine", "hbs");
+
+//- set middleware
+app.use(express.urlencoded({ extended: true }));
+
+//- set route
+app.get("/", (req, res) => {
+  return res.render("index");
+});
+//- listen to server
+app.listen(port, () => {
+  console.log(`Server is listening to http://localhost:${port}`);
+});
