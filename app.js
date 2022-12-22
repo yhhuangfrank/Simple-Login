18a8940dcfa6a7e69acf96a3acd135d1e8cee2a4
frank <frankcool321@gmail.com>
2022-12-22 15:01:52 +0800

    feat: add formValidator

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 
-
 //- parameter setting
 const app = express();
 const port = 3000;
@@ -13,11 +12,14 @@ app.set("view engine", "hbs");
 
 //- set middleware
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"))
 
 //- set route
 app.get("/", (req, res) => {
   return res.render("index");
 });
+
+
 //- listen to server
 app.listen(port, () => {
   console.log(`Server is listening to http://localhost:${port}`);
---
+++ public/javascript/formValidator.js
@@ -0,0 +1,9 @@
+const LoginForm = document.querySelector("#LoginForm");
+
+LoginForm.addEventListener("submit", function onFormSubmitted(e) {
+  if (!LoginForm.checkValidity()) {
+    e.preventDefault();
+    e.stopPropagation();
+  }
+  LoginForm.classList.add("was-validated")
+});
